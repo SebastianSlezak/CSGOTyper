0fb2b749c8aa38cc5127ad37fc54ead32faaa8aa
SebastianSlezak <szalassje@gmail.com>
2023-03-29 21:19:22 +0200

    version 0.0.4 - Error 403 has been fixed

--- src/main/java/app/TyperControl.java
+++ src/main/java/app/TyperControl.java
@@ -19,8 +19,9 @@ public class TyperControl {
     public void typeMatch(String url){
 
         try {
-            Document response = Jsoup.connect(url).get();
-            System.out.println(response);
+            String strText = Jsoup.connect(url).referrer("http://www.google.com").ignoreHttpErrors(true).get().text();
+//            Document response = Jsoup.connect(url).ignoreHttpErrors(true).timeout(5000).get();
+            System.out.println(strText);
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
